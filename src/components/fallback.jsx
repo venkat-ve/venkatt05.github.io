46870a36a0db706e3a7b17a51350252c50859458
Venkat <75605991+venkatt05@users.noreply.github.com>
2021-05-10 22:38:20 +0530

    init commit

--- src/components/fallback.jsx
+++
@@ -1,9 +0,0 @@
-import logo from "../logo.svg";
-
-export default function FallBack() {
-  return (
-    <div className="Fallback-section">
-      <img src={logo} className="App-logo fallback-wrap" alt="logo" />
-    </div>
-  );
-}
